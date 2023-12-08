37d88fef94b9b8fb13dc7af710ad71ca5004e5b9
alison.jung <alison.jung@kakaocorp.com>
2023-12-08 17:25:41 +0900

    feat : 배포 환경 설정 추가 ci : github workflow 업데이트 docs : README.md 업데이트 chore : 일부 명칭 수정(i.e : stage->stg)

--- run_application.sh
+++ run_application.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 if [[ $GITHUB_EVENT_NAME == 'push' ]]; then
-    if [[ $GITHUB_REF == 'refs/tags/*stage-health-check' ]]; then
+    if [[ $GITHUB_REF == 'refs/tags/*.dev-health-check' ]]; then
+          SPRING_PROFILES_ACTIVE=dev
+    elif [[ $GITHUB_REF == 'refs/tags/*.stg-health-check' ]]; then
         SPRING_PROFILES_ACTIVE=stage
-    elif [[ $GITHUB_REF == 'refs/tags/*main-health-check' ]]; then
+    elif [[ $GITHUB_REF == 'refs/tags/*.prod-health-check' ]]; then
         SPRING_PROFILES_ACTIVE=prod
     else
-        SPRING_PROFILES_ACTIVE=dev
+        SPRING_PROFILES_ACTIVE=local
     fi
 fi
 
 # 만약 해당 환경 변수가 이미 설정되어 있다면 건드리지 않음
 if [ -z "$SPRING_PROFILES_ACTIVE" ]; then
-    SPRING_PROFILES_ACTIVE=dev
+    SPRING_PROFILES_ACTIVE=local
 fi
 
 # Spring Boot 애플리케이션 실행
